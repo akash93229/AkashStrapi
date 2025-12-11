99b5164cf3e33a15c1d471c0791f69a042234109
Akash@123 <55844230+akash93229@users.noreply.github.com>
2025-12-11 17:57:26 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,22 @@
+# provider.tf
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.5.0"
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "terraform.tfstate"
+    region         = var.region
+    dynamodb_table = var.dynamodb_table
+  }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
