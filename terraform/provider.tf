d4402966b236f204385b01f294ef0b68371e9f47
Akash@123 <55844230+akash93229@users.noreply.github.com>
2025-12-11 18:23:53 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,14 +8,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "my-strapi-terraform-state"     # HARD CODE
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"                    # HARD CODE
-    dynamodb_table = "terraform-state-lock"          # HARD CODE
-  }
-}
-
 provider "aws" {
   region = var.region
 }
