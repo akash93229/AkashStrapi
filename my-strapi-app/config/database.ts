211a2cc3ba58da81b357dcd84067aeb3b40acb33
Ubuntu <ubuntu@ip-172-31-22-183.ap-south-1.compute.internal>
2025-12-11 11:12:03 +0000

    Include Strapi app in main repo as regular files

---
+++ my-strapi-app/config/database.ts
@@ -0,0 +1,11 @@
+import path from 'path';
+
+export default () => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', '..', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+  },
+}); 
